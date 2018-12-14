83f506ef7361e2d8471d64ccbab5767a9520bcdf
Paulo Suderio <paulo.suderio@bndes.gov.br>
2018-12-14 18:15:18 -0200

    lambdas

--- src/main/java/net/technearts/moto/Lambda.java
+++ src/main/java/net/technearts/moto/Lambda.java
@@ -1,32 +1,154 @@
 package net.technearts.moto;
 
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Set;
 
-public abstract class Lambda {
-  public abstract Object apply(List<Object> params);
+import groovy.lang.Binding;
+import groovy.lang.GroovyShell;
+import lombok.Data;
 
-  public abstract List<Object> paramsOk(List<Object> params);
+@Data
+public class Lambda<R> {
+  private String name;
+  private List<Parameter> parameters = new ArrayList<>();
+  private String varargType;
+  private String resultType;
+  private String script;
+  private GroovyShell shell;
+  private Binding binding;
+  private Set<Lambda<?>> dependencies;
 
-  public abstract List<Object> paramsNotOk(List<Object> params);
-
-  public abstract List<Object> paramsMissing(List<Object> params);
-
-  public Object apply(Object... params) {
-    return apply(Arrays.asList(params));
+  public Lambda() {
+    this.binding = new Binding();
+    this.shell = new GroovyShell(binding);
+    this.dependencies = new HashSet<>();
   }
 
-  public List<Object> paramsOk(Object... params) {
-    return paramsOk(Arrays.asList(params));
+  @SuppressWarnings("unchecked")
+  public R evaluate(Map<String, Object> params, List<Object> varargs) {
+    if (params.size() != parameters.size()) {
+      throw new IllegalArgumentException(
+          "Parâmetros enviados não correspondem aos definidos na função.");
+    }
+    StringBuilder sb = new StringBuilder();
+    sb.append(this);
+    sb.append(name).append('(');
+    for (Parameter p : parameters) {
+      if (!params.containsKey(p.getName())) {
+        throw new IllegalArgumentException("Parâmetro " + p.getName() + " está faltando");
+      }
+      sb.append("param_").append(p.getName()).append(',');
+      binding.setVariable("param_" + p.getName(), params.get(p.getName()));
+    }
+    sb.setCharAt(sb.length() - 1, ')');
+    System.out.println(sb);
+    return (R) shell.evaluate(sb.toString());
   }
 
-  public List<Object> paramsNotOk(Object... params) {
-    return paramsNotOk(Arrays.asList(params));
+  public String toString() {
+    StringBuilder params = new StringBuilder();
+    for (Parameter p : parameters) {
+      params.append(p.getType()).append(' ').append(p.getName()).append(", ");
+    }
+    params.setLength(params.length() - 2);
+    String result = "";
+    for (Lambda<?> dependency : dependencies) {
+      result += dependency;
+    }
+    result += String.format("%s %s(%s) {\n%s\n}\n", resultType, name, params, script);
+    return result;
   }
 
-  public List<Object> paramsMissing(Object... params) {
-    return paramsMissing(Arrays.asList(params));
+  public static <T> Lambda<T> builder(String name, Class<T> resultType) {
+    Lambda<T> result = new Lambda<>();
+    result.setName(name);
+    result.setResultType(resultType.getSimpleName());
+    return result;
   }
 
-  public abstract List<Class<?>> allParams();
+  public <T> Lambda<R> param(String name, Class<T> klazz) {
+    Parameter parameter = new Parameter();
+    parameter.setName(name);
+    parameter.setType(klazz.getSimpleName());
+    parameters.add(parameter);
+    return this;
+  }
+
+  public Lambda<R> returns(String script) {
+    this.script = script;
+    return this;
+  }
+
+  public Lambda<R> uses(Lambda<?>... dependencies) {
+    for (Lambda<?> dependency : dependencies) {
+      this.dependencies.add(dependency);
+    }
+    this.dependencies.addAll(dependencies());
+    return this;
+  }
+
+  private Set<Lambda<?>> dependencies() {
+    Set<Lambda<?>> result = new HashSet<>();
+    for (Lambda<?> dependency : this.dependencies) {
+      result.addAll(dependency.dependencies());
+    }
+    result.addAll(this.dependencies);
+    return result;
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj) {
+      return true;
+    }
+    if (obj == null) {
+      return false;
+    }
+    if (!(obj instanceof Lambda)) {
+      return false;
+    }
+    Lambda other = (Lambda) obj;
+    boolean sameParameters = false;
+    for (Parameter p : parameters) {
+      sameParameters
+    }
+    return Objects.equals(name, other.name) && Objects.equals(parameters, other.parameters);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(name, parameters);
+  }
 }
+
+
+@Data
+class Parameter {
+  private String name;
+  private String type;
+
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj) {
+      return true;
+    }
+    if (obj == null) {
+      return false;
+    }
+    if (!(obj instanceof Parameter)) {
+      return false;
+    }
+    Parameter other = (Parameter) obj;
+    return Objects.equals(type, other.type);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(type);
+  }
+}
+
--- src/test/java/net/technearts/moto/LambdaTest.java
+++ src/test/java/net/technearts/moto/LambdaTest.java
@@ -1,28 +1,45 @@
 package net.technearts.moto;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.math.BigDecimal;
-import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.junit.Test;
 
 public class LambdaTest {
 
   @Test
-  public void test() {
+  public void simpleTest() {
+    Lambda<BigDecimal> sub = Lambda.builder("sub", BigDecimal.class).param("a", BigDecimal.class)
+        .param("b", BigDecimal.class).returns("a - b");
+    Map<String, Object> params = new HashMap<>();
+    params.put("a", new BigDecimal(7));
+    params.put("b", new BigDecimal(3));
+    BigDecimal result = sub.evaluate(params, null);
+    assertEquals(new BigDecimal(4), result);
 
+    params = new HashMap<>();
+    params.put("a", new BigDecimal(1));
+    params.put("b", new BigDecimal(1));
+    result = sub.evaluate(params, null);
+    assertEquals(new BigDecimal(0), result);
   }
-/**
- * sum = a + b;
- * 
- * sub = a - b;
- * 
- * sumsub = sum(sub(a,b), sub(c,d));
- * 
- * sumsub(a, b, c, d);
- * 
- * 
- */
+
+  @Test
+  public void dependencyTest() {
+    Lambda<BigDecimal> sum = Lambda.builder("sum", BigDecimal.class).param("a", BigDecimal.class)
+        .param("b", BigDecimal.class).returns("a + b");
+    Lambda<BigDecimal> sub = Lambda.builder("sub", BigDecimal.class).uses(sum).param("a", BigDecimal.class)
+        .param("b", BigDecimal.class).returns("a - b");
+    Lambda<BigDecimal> formula = Lambda.builder("formula", BigDecimal.class)
+        .param("a", BigDecimal.class).param("b", BigDecimal.class).uses(sum, sub).returns("sum(a, b) - sub(a, b)");
+    Map<String, Object> params = new HashMap<>();
+    params.put("a", new BigDecimal(7));
+    params.put("b", new BigDecimal(3));
+    BigDecimal result = formula.evaluate(params, null);
+    assertEquals(new BigDecimal(6), result);
+  }
+
 }
